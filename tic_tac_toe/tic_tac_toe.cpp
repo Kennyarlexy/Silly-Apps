824889d9c0d5acd88ac2c15b36369c6a2a9deb92
kennyarlexy <kennyarlexy@gmail.com>
2022-04-19 13:13:20 +0700

    removed redundancy, made codes cleaner and more maintainable

--- tic_tac_toe/tic_tac_toe.cpp
+++ tic_tac_toe/tic_tac_toe.cpp
@@ -3,15 +3,9 @@ using namespace std;
 
 class Game {
     string grid[7];
-    vector<int> elementInRow;
-    vector<int> elementInCol;
-    vector<int> elementInDiagonal; //diagonal[1] = \ direction, diagonal[2] = / direction
 
 public:
     Game() {
-        this->elementInRow = vector<int> (4, 0);
-        this->elementInCol = vector<int> (4, 0);
-        this->elementInDiagonal = vector<int> (3, 0);
         for (int r = 0; r < 7; r++) {
             if (r % 2 == 0) {
                 this->grid[r] = "+---+---+---+";
@@ -21,18 +15,13 @@ public:
         }
     }
 
-    int getElementInRow(int row);
-    int getElementInCol(int col);
-    int getElementInDiagonal(int diagonal);
-
     friend class Player;
     friend ostream& operator << (ostream& output, Game board);
     bool evaluateRow(int row);
     bool evaluateCol(int col);
-    bool evaluateDiagonal(int diagonal);
+    bool evaluateDiagonal();
     bool canChoose(int col, int row);
     bool isOver(int col, int row);
-    void debug();
 };
 
 class Player {
@@ -54,24 +43,6 @@ public:
 };
 
 //Game::function() definition----------------------------------------------------------------------------------
-int Game::getElementInRow(int row) {
-    return elementInRow[row];
-}
-
-int Game::getElementInCol(int col) {
-    return elementInCol[col];
-}
-
-int Game::getElementInDiagonal(int diagonal) {
-    return elementInDiagonal[diagonal];
-}
-
-bool Game::canChoose(int col, int row) {
-    int r = row*2 - 1;
-    int c = col*4 - 2;
-    return (this->grid[r][c] == ' ');
-}
-
 ostream& operator << (ostream& output, Game board) {
     for (auto& row : board.grid) {
         output << row << "\n";
@@ -90,34 +61,25 @@ bool Game::evaluateCol(int col) {
     return (grid[3][c] == grid[1][c] && grid[3][c] == grid[5][c]);
 }
 
-bool Game::evaluateDiagonal(int diagonal) {
-    if (diagonal == 1) {
-        return (grid[3][6] == grid[1][2] && grid[3][6] == grid[5][10]);
-    } else {
-        return (grid[3][6] == grid[5][2] && grid[3][6] == grid[1][10]);
-    }
+bool Game::evaluateDiagonal() {
+    if (grid[3][6] == ' ') return false;
+
+    return (
+        (grid[3][6] == grid[1][2] && grid[3][6] == grid[5][10]) ||
+        (grid[3][6] == grid[5][2] && grid[3][6] == grid[1][10])
+    );
+}
+
+bool Game::canChoose(int col, int row) {
+    int r = row*2 - 1;
+    int c = col*4 - 2;
+    return (this->grid[r][c] == ' ');
 }
 
 bool Game::isOver(int col, int row) {
     return (
-        (this->getElementInCol(col) == 3 && this->evaluateCol(col)) ||
-        (this->getElementInRow(row) == 3 && this->evaluateRow(row)) ||
-        (this->getElementInDiagonal(1) == 3 && this->evaluateDiagonal(1)) ||
-        (this->getElementInDiagonal(2) == 3 && this->evaluateDiagonal(2))
+        this->evaluateCol(col) || this->evaluateRow(row) || this->evaluateDiagonal()
     );
-}
-
-void Game::debug() {
-    cout << *this << "\n";
-    for (int r = 1; r <= 3; r++) {
-        cout << "Elements in row " << r << " = " << elementInRow[r] << "\n";
-    }
-    for (int c = 1; c <= 3; c++) {
-        cout << "Elements in col " << c << " = " << elementInCol[c] << "\n";
-    }
-    for (int d = 1; d <= 2; d++) {
-        cout << "Elements in diagonal " << d << " = " << elementInDiagonal[d] << "\n";
-    }
 }
 
 //Player::function() definition--------------------------------------------------------------------------------
@@ -138,12 +100,7 @@ string Player::getName() {
 void Player::choose(int col, int row) {
     int r = row*2 - 1;
     int c = col*4 - 2;
-
     this->game->grid[r][c] = this->symbol;
-    this->game->elementInRow[row]++;
-    this->game->elementInCol[col]++;
-    if (col == row) this->game->elementInDiagonal[1]++;
-    if (col == 4 - row) this->game->elementInDiagonal[2]++;
 }
 
 //global function definition-----------------------------------------------------------------------------------
@@ -157,15 +114,11 @@ void play(Player &player_1, Player &player_2, bool turn) {
     while (true) {
         round++;
         Player *currentPlayer;
-        if (turn) {
-            currentPlayer = (round % 2 == 1 ? &player_1 : &player_2);
-        } else {
-            currentPlayer = (round % 2 == 1 ? &player_2 : &player_1);
-        }
+        if (turn)   currentPlayer = (round % 2 == 1 ? &player_1 : &player_2);
+        else        currentPlayer = (round % 2 == 1 ? &player_2 : &player_1);
         cout << currentPlayer->getName() << "'s turn!\n";
 
         int col, row;
-
         //process input
         while (true) {
             cout << "Choose position: ";
